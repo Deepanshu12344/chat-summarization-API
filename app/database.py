ce5e631627c522bcaf6fa022eb476cdf87c688fe
Deepanshu12344 <deepanshu123sharma4@gmail.com>
2025-07-02 23:16:02 +0530

    changed mongodb uri

--- app/database.py
+++ app/database.py
@@ -1,16 +1,3 @@
-# from motor.motor_asyncio import AsyncIOMotorClient
-# from pymongo import ASCENDING
-# import os
-
-# MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-# client = AsyncIOMotorClient(MONGO_URI)
-# db = client["chatdb"]
-
-# async def init_indexes():
-#     await db.chats.create_index([("user_id", ASCENDING)])
-#     await db.chats.create_index([("conversation_id", ASCENDING)])
-#     await db.chats.create_index([("timestamp", ASCENDING)])
-
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo import ASCENDING
 import os
@@ -24,11 +11,7 @@ if not MONGO_URI:
 
 client = AsyncIOMotorClient(MONGO_URI)
 
-default_db = client.get_default_database()
-if default_db is not None:
-    db = default_db
-else:
-    db = client["chatdb"]  # Fallback to manually named DB
+db = client["chatdb"]  # Fallback to manually named DB
 
 async def init_indexes():
     await db.chats.create_index([("user_id", ASCENDING)])
